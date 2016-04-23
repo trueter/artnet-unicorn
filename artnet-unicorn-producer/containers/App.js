d989b5986e42ac3b96773f2014c8e9df67a5db39
Torsten Rü <torsten@rueter.net>
2016-04-23 19:56:57 +0200

    remove boilerplate stuff

--- artnet-unicorn-producer/containers/App.js
+++ artnet-unicorn-producer/containers/App.js
@@ -1,11 +1,11 @@
 import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import * as TodoActions from '../actions'
+// import * as TodoActions from '../actions'
 import FixtureCanvas from '../components/FixtureCanvas'
 import KeyFrameCanvas from '../components/KeyFrameCanvas'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     const { todos, actions } = this.props
     return (
@@ -19,25 +19,3 @@ class App extends Component {
     )
   }
 }
-
-App.propTypes = {
-  todos: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
-}
-
-function mapStateToProps(state) {
-  return {
-    todos: state.todos
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(TodoActions, dispatch)
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
