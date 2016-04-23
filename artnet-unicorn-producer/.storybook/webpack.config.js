46138492fbcfd912b8dd00aa3616317ac5eef420
Torsten Rü <torsten@rueter.net>
2016-04-23 20:20:13 +0200

    fix webpack babel state

--- artnet-unicorn-producer/.storybook/webpack.config.js
+++ artnet-unicorn-producer/.storybook/webpack.config.js
@@ -4,6 +4,15 @@ module.exports = {
   module: {
     loaders: [
       {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, '../'),
+        query: {
+          presets: ['es2015', 'react', 'stage-0', 'stage-1']
+        }
+      },
+      {
         test: /\.css?$/,
         loaders: [ 'style', 'raw' ],
         include: path.resolve(__dirname, '../')
